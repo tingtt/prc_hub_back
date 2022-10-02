18eb68336fa4cbc37b6127c05a93a2029fedaaa5
tingtt <ting.taku@gmail.com>
2022-10-03 03:09:58 +0900

    misc: docker環境のセットアップ

---
+++ .mysql/init.sql
@@ -0,0 +1,14 @@
+SET
+  SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
+START TRANSACTION;
+
+SET
+  time_zone = "+00:00";
+
+--
+-- Database: `prc_hub`
+--
+CREATE DATABASE IF NOT EXISTS `prc_hub` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+USE `prc_hub`;
