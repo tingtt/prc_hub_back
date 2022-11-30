6f950c6d0e82010c71e462ffe060f5de529a4ec8
tingtt <ting.taku@gmail.com>
2022-11-30 14:57:06 +0900

    fix(User): fix invalid password response

--- domain/model/user/verify.go
+++ domain/model/user/verify.go
@@ -7,6 +7,9 @@ import (
 // パスワード検証
 func (u *User) Verify(password string) (verify bool, err error) {
 	err = bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
+		return false, nil
+	}
 	if err != nil {
 		return false, err
 	}
